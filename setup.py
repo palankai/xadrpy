bd061bf8b7894f61e1debfed98fa60cea0742fa2
Csaba Palankai <csaba.palankai@gmail.com>
2012-03-10 07:09:57 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import os
+import sys
+import codecs
+
+def read(fname):
+    return codecs.open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='xadrpy',
+    packages=find_packages('src'),
+    package_dir={'':'src'},
+    include_package_data=True,    
+    version=read('VERSION').strip(),
+    description="Django tool",
+    long_description=read('README.rst'),
+    classifiers=[
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        'Natural Language :: English',
+        'Development Status :: Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Tools',
+    ],
+    keywords=('django'),
+    author='xadrpy Contributors',
+    author_email='csaba.palankai@gmail.com',
+    maintainer = 'Csaba Palankai',
+    maintainer_email = 'csaba.palankai@gmail.com',
+    url='https://github.com/pacsee/xadrpy',
+    license='GNU LGPL',
+    zip_safe=False,
+    install_requires=[
+    ],)
