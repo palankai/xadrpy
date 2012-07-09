9fffe25c632d2724d37fad6e25195499f889e549
Palánkai Csaba <csaba.palankai@gmail.com>
2012-07-09 15:50:56 +0000

    Add: auth backends

--- src/xadrpy/auth/backends.py
+++ src/xadrpy/auth/backends.py
@@ -1,5 +1,20 @@
-'''
-Created on 2012.07.06.
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth.models import User
 
-@author: pcsaba
-'''
+class EmailBackend(ModelBackend):
+    def authenticate(self, username=None, password=None):
+        try:
+            for user in User.objects.filter(email=username):
+                if user.check_password(password):
+                    return user
+        except User.DoesNotExist:
+            pass
+        return None
+
+class UserBackend(ModelBackend):
+    def authenticate(self, user_id=None, user=None ):
+        if user:
+            return user
+        if not user_id:
+            return None
+        return self.get_user(user_id)
