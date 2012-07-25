3b354fb8cef09f0dfb3ecf0330f77d2286a92d7b
Csaba Palankai <csaba.palankai@gmail.com>
2012-07-25 11:33:12 +0200

    Fix: Pref fallback error

--- src/xadrpy/auth/templatetags/prefs.py
+++ src/xadrpy/auth/templatetags/prefs.py
@@ -9,7 +9,7 @@ register = WidgetLibrary()
 class PropertyNode(XWidgetBase):
 
     def value(self, context, key=None, instance=None, site=None, consumer=None, account=None, rule=None, role=None, group=None, user=None, access=None, token=None, custom=None, namespace=None, language_code=None, default=None, trans=None, order=""):
-        order = [item.strip() for item in order.split(",")]
+        order = [item.strip() for item in order.split(",") if item.strip()]
         v = prefs(key=key, instance=instance, site=site, consumer=consumer, account=account, rule=rule, role=role, group=group, user=user, access=access, token=token, custom=custom, namespace=namespace, language_code=language_code, default=default, trans=trans, order=order)
         if not self.asvar:
             v = v or ""
